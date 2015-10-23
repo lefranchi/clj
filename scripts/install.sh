0541d1ee2c2920e15855e4b2c4903a9af6b8706a
Leandro Franchi <leandro.franchi@gmail.com>
2015-10-22 22:55:31 -0200

    Retirando geração de daemon. Não é compatível com armv7l. Gerando programa shell.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,44 +1,20 @@
 #! /bin/sh
 
-mvn clean install -Denvironment=production
+echo "INICIO ====================================================================="
+
+mvn clean package appassembler:assemble -Denvironment=production
 
 echo "Removendo Diretorio CLJ..."
 rm -rf /opt/ablebit/clj
 
-echo "Criando Diretorio CLJ..."
-mkdir -p /opt/ablebit/clj
+echo "Criando Diretorio ABLEBIT..."
+mkdir -p /opt/ablebit/
 
 echo "Copiando Diretorio CLJ..."
-cp -r target/clj/jsw/* /opt/ablebit/clj/
-
-echo "====================================================================="
-echo "Configurando Receptor"
-echo "====================================================================="
-
-echo "Criando diretório de Logs..."
-mkdir -p /opt/ablebit/clj/receptor/logs/
-
-echo "Copiando Arquivos de Configuracoes..."
-cp src/main/resources/clj.properties /opt/ablebit/clj/receptor/conf/
-cp src/main/resources/log4j.xml /opt/ablebit/clj/receptor/conf/
+cp -r target/clj /opt/ablebit/
 
 echo "Setando Permissoes..."
-chmod +x /opt/ablebit/clj/receptor/bin/receptor
-chmod +x /opt/ablebit/clj/receptor/bin/wrapper-linux-x86-64
+chmod +x /opt/ablebit/clj/bin/receptor
+chmod +x /opt/ablebit/clj/bin/transmissor
 
-echo "====================================================================="
-echo "Configurando Transmissor"
-echo "====================================================================="
-
-echo "Criando diretório de Logs..."
-mkdir -p /opt/ablebit/clj/transmissor/logs/
-
-echo "Copiando Arquivos de Configuracoes..."
-cp src/main/resources/clj.properties /opt/ablebit/clj/transmissor/conf/
-cp src/main/resources/log4j.xml /opt/ablebit/clj/transmissor/conf/
-
-echo "Setando Permissoes..."
-chmod +x /opt/ablebit/clj/transmissor/bin/transmissor
-chmod +x /opt/ablebit/clj/transmissor/bin/wrapper-linux-x86-64
-
-echo "====================================================================="
+echo "FIM ====================================================================="
