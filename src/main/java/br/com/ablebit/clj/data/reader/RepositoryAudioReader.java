efd203d1bf0d138fe5b14cb1e6f8536c2d22007b
Leandro Franchi <leandro.franchi@gmail.com>
2015-10-23 14:01:37 -0200

    Criação de leitores e escritores de dados de audio nos repositórios.

---
+++ src/main/java/br/com/ablebit/clj/data/reader/RepositoryAudioReader.java
@@ -0,0 +1,112 @@
+package br.com.ablebit.clj.data.reader;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.SourceDataLine;
+
+import org.apache.log4j.Logger;
+
+import br.com.ablebit.clj.data.Repository;
+import br.com.ablebit.clj.net.Packet;
+
+
+/**
+ * Le pacotes do Buffer Central e faz PlayBack.
+ * 
+ * @author leandro.franchi
+ *
+ */
+public class RepositoryAudioReader implements Runnable {
+
+	/**
+	 * Logger.
+	 */
+	private static final Logger LOG = Logger.getLogger(RepositoryAudioReader.class);
+	
+	/**
+	 * Source DataLine.
+	 */
+	private SourceDataLine sourceDataLine;
+	
+	/**
+	 * Audio Format.
+	 */
+	private AudioFormat audioFormat;
+	
+	/**
+	 * Repositorio de onde serao lidos os pacotes recebidos e enviados a saida de audio.
+	 */
+	private Repository<Packet> repository;
+	
+	/**
+	 * Controle de play. Nao pode tocar menor que o ultimo tocado;
+	 */
+	private long maxPacketCounter;
+
+	/**
+	 * Construtor Padrao. Ja inicializa Mixers e Lines de Audio de acordo a configuracao.
+	 * 
+	 * @param repository
+	 * 
+	 */
+	public RepositoryAudioReader(Repository<Packet> repository, float sampleRate, int sampleSize, int channels, boolean signed, boolean bigEndian) {
+		
+		this.setRepository(repository);
+		
+		try {
+			
+			this.audioFormat = new AudioFormat(sampleRate, sampleSize, channels, signed, bigEndian);
+				
+			DataLine.Info info = new DataLine.Info(SourceDataLine.class, this.audioFormat);
+			sourceDataLine = (SourceDataLine) AudioSystem.getLine(info);
+            sourceDataLine.open(this.audioFormat, sourceDataLine.getBufferSize());
+			sourceDataLine.start();
+            
+            LOG.info("Leitor de Repositorio para Audio Instanciado!");
+            
+		} catch(Exception e) {
+			LOG.fatal("Impossivel abrir interface de audio.", e);
+			System.exit(-1);
+		}
+		
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see java.lang.Runnable#run()
+	 */
+	@Override
+	public void run() {
+		
+		while(!Thread.currentThread().isInterrupted()) {
+			
+			try {
+				
+				Packet packet = getRepository().take();
+				
+				if (packet.getCounter() > maxPacketCounter) {
+					LOG.info("LENDO==" + packet.getCounter() + " - Tamanho: " + packet.getContent().length );
+					sourceDataLine.write(packet.getContent(), 0 , packet.getContent().length); 
+					maxPacketCounter = packet.getCounter();
+				} else {
+					LOG.info("DESCARTANDO==" + packet.getCounter() + " - Tamanho: " + packet.getContent().length );					
+				}
+				
+			} catch (InterruptedException e) {
+				LOG.fatal("Erro na leitura no repositorio!", e);
+			}
+			
+		}
+		
+	}
+
+	public Repository<Packet> getRepository() {
+		return repository;
+	}
+
+	public void setRepository(Repository<Packet> repository) {
+		this.repository = repository;
+	}
+
+}
---
+++ src/main/java/br/com/ablebit/clj/data/writer/RepositoryAudioWriter.java
@@ -0,0 +1,111 @@
+package br.com.ablebit.clj.data.writer;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.TargetDataLine;
+
+import org.apache.log4j.Logger;
+
+import br.com.ablebit.clj.data.Repository;
+import br.com.ablebit.clj.net.Packet;
+
+/**
+ * Insere pacotes no repositorio a partir de um device.
+ * 
+ * @author leandro.franchi
+ *
+ */
+public class RepositoryAudioWriter implements Runnable {
+	
+	/**
+	 * Logger.
+	 */
+	private static final Logger LOG = Logger.getLogger(RepositoryAudioWriter.class);
+	
+	/**
+	 * Audio Target DataLine.
+	 */
+	private TargetDataLine targetDataLine;
+	
+	/**
+	 * Audio Format.
+	 */
+	private AudioFormat audioFormat;
+	
+	/**
+	 * Reporitorio que contem os pacotes a serem transmitidos.
+	 */
+	private Repository<Packet> repository;
+	
+	/**
+	 * Auxiliar para contador de pacotes.
+	 */
+	private long packetCounter = 0;
+	
+	/**
+	 * Construtor Padrao. Ja inicializa Mixers e Lines de Audio de acordo a configuracao.
+	 * 
+	 * @param configuration
+	 */
+	public RepositoryAudioWriter(Repository<Packet> repository, float sampleRate, int sampleSize, int channels, boolean signed, boolean bigEndian) {
+
+		setRepository(repository);
+		
+		try {
+			
+			this.audioFormat = new AudioFormat(sampleRate, sampleSize, channels, signed, bigEndian);
+
+			DataLine.Info info = new DataLine.Info(TargetDataLine.class, this.audioFormat);
+			targetDataLine = (TargetDataLine) AudioSystem.getLine(info);
+			targetDataLine.open(this.audioFormat, targetDataLine.getBufferSize());
+			targetDataLine.start();
+			
+			LOG.info("Escritor de Repositorio para Audio Instanciado!");
+			
+		} catch(Exception e) {
+			LOG.fatal("Impossivel abrir interface de audio.", e);
+			System.exit(-1);
+		}
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see java.lang.Runnable#run()
+	 */
+	@Override
+	public void run() {
+		
+		while(!Thread.currentThread().isInterrupted()) {
+			
+			int frameSizeInBytes = this.audioFormat.getFrameSize();
+	        int bufferLengthInFrames = targetDataLine.getBufferSize() / 8;
+	        int bufferLengthInBytes = bufferLengthInFrames * frameSizeInBytes;
+	        byte[] data = new byte[bufferLengthInBytes];
+	        
+			if(targetDataLine.read(data, 0, bufferLengthInBytes) != -1) {
+
+				byte packetBuffer[] = new byte[bufferLengthInBytes];
+				System.arraycopy(data, 0, packetBuffer, 0, bufferLengthInBytes);
+
+				Packet packet = new Packet(packetCounter++, packetBuffer);
+				getRepository().put(packet);
+				
+	        }
+			
+		}
+			
+	}
+
+
+	public Repository<Packet> getRepository() {
+		return repository;
+	}
+
+
+	public void setRepository(Repository<Packet> repository) {
+		this.repository = repository;
+	}
+
+}
